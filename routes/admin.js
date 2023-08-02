c3ec42d3ea25aa28745a0c86f78ffdf85ad598e0
Saurabh-Rana17 <saurabhrana200317@gmail.com>
2023-08-03 00:04:35 +0530

    html served

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,8 @@
 const express = require("express");
+const path = require("path");
 const router = express.Router();
 router.get("/add-product", (req, res, next) => {
-  res.send(
-    "<form action='/admin/products' method='POST'><input type='text' name='i1'><button type='submit'>submit</button></form>"
-  );
+  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
 });
 router.post("/products", (req, res) => {
   const data = req.body;
