46861bb45bb42cf77d1e6814740f1b3aeea2a2e7
Saurabh-Rana17 <saurabhrana200317@gmail.com>
2023-08-03 00:58:01 +0530

    css + refactor

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const http = require("http");
+const path = require("path");
 const adminRoutes = require("./routes/admin");
 const bodyParser = require("body-parser");
 const express = require("express");
 const shopRoutes = require("./routes/shop");
 const app = express();
+app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res) => {
-  res.sendFile(__dirname + "/views/pg404.html");
+  res.sendFile(__dirname + "/views/404.html");
 });
 
 app.listen(3000);
