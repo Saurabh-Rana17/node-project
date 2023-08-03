2b8ff7a65853cfac15c70eda132f9f54b98fe8c9
Saurabh-Rana17 <saurabhrana200317@gmail.com>
2023-08-03 23:50:40 +0530

    pug add-product page added

--- routes/shop.js
+++ routes/shop.js
@@ -3,7 +3,8 @@ const express = require("express");
 const router = express.Router();
 const adminData = require("./admin");
 router.get("/", (req, res, next) => {
-  res.render("shop");
+  const products = adminData.products;
+  res.render("shop", { docTitle: "shop", prods: products });
 });
 
 module.exports = router;
