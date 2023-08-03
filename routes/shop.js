40469b13f87ef041efe4f6aee9d4079d3276f48d
Saurabh-Rana17 <saurabhrana200317@gmail.com>
2023-08-03 22:57:03 +0530

    pug rendered

--- routes/shop.js
+++ routes/shop.js
@@ -3,8 +3,7 @@ const express = require("express");
 const router = express.Router();
 const adminData = require("./admin");
 router.get("/", (req, res, next) => {
-  console.log(adminData.products);
-  res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
+  res.render("shop");
 });
 
 module.exports = router;
