2d77dfd9f0a4f51621e1c8be0cfdca3c2fb124fd
Saurabh-Rana17 <saurabhrana200317@gmail.com>
2023-08-07 23:54:41 +0530

    products added

--- models/product.js
+++ models/product.js
@@ -1,12 +1,36 @@
+const { log } = require("console");
+const { json } = require("express");
+const fs = require("fs");
+const path = require("path");
 const products = [];
 module.exports = class Product {
   constructor(t) {
     this.title = t;
   }
   save() {
-    products.push(this);
+    const p = path.join(
+      path.dirname(require.main.filename),
+      "data",
+      "products.json"
+    );
+    fs.readFile(p, (err, fileContent) => {
+      let products = [];
+      if (!err) {
+        products = JSON.parse(fileContent);
+      }
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
   }
   static fetchall() {
+    fs.readFile(p, (err, fileContent) => {
+      if (err) {
+        return [];
+      }
+      return JSON.parse(fileContent);
+    });
     return products;
   }
 };
